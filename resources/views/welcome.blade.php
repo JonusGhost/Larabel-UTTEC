4965e386ef4dde4f7a4c6aeb01ca2afc9ea2a780
Jona <jonathan.emmanuel604@gmail.com>
2024-08-07 02:15:01 -0600

    0Auth fuera

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -119,10 +119,7 @@
                             <a href="{{ route('register') }}" class="btn">Register</a>
                         @endif
                     @endauth
-                    <a href="{{ url('/auth/redirect') }}" class="btn">
-        <img src="{{ asset('images/imgGoogle.png') }}" alt="Google Logo" class="google-logo">
-        Iniciar sesión con Google
-    </a>
+                    
 
                 </nav>
             @endif
